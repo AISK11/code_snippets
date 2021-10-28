7b206bcaea538dbcbea2bdd756185612bad0ff2e
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-10-28 17:32:36 +0200

    Create web_get_html.py

---
+++ python/web_get_html.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+###############################################
+# Author: AISK11                              #
+# Description: this snippet returns html code #
+# as a string when provided with a valid URL. #
+###############################################
+
+## Library for handling web requests:
+import urllib.request, urllib.error
+## Library for handling outpur to stderr and exit code:
+import sys
+## Library for returning exit codes as defined in POSIX:
+## https://docs.python.org/2/library/os.html#process-management
+import os
+
+def web_get_html(url):
+    ## Try to open requested URL:
+    try:
+        ## HTTPResponse object:
+        req = urllib.request.urlopen(url)
+    ## Exception: HTTP status code (e.g. 404, 502, ...):
+    except urllib.error.HTTPError as e:
+        print(f"HTTPError: {e.code}", file=sys.stderr)
+        sys.exit(os.EX_UNAVAILABLE)
+    ## Exception: most likely wrong URL:
+    except urllib.error.URLError as e:
+        print(f"URLError: {e.reason}", file=sys.stderr)
+        sys.exit(os.EX_NOHOST)
+
+    ## Read bytes from HTTPResponse and then decode from bytes to string
+    html = req.read().decode("utf8")
+
+    ## Return HTML code
+    return html
+
+
+html = web_get_html("http://www.python3.com") ## CHANGE THIS
+print(f"{html}")
+
