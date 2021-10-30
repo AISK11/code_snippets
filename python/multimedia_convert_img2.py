a09b9a4ac24a65e7aab8cebe21e4c1b14dfb2caa
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-10-30 03:01:59 +0200

    Add files via upload

--- python/multimedia_convert_img2.py
+++ python/multimedia_convert_img2.py
@@ -32,7 +32,11 @@ from PIL import Image
 #########
 #  BMP  #
 #########
-def multimedia_convert_img_to_bmp(img_input, img_output=''.join(img_input.split(".")[:-1])+".bmp"):
+def multimedia_convert_img_to_bmp(img_input, img_output=None):
+
+    ## If 'img_output' was not set by user, use the same name as 'img_input', just change extension:
+    if img_output == None:
+        img_output = ''.join(img_input.split(".")[:-1])+".bmp"
 
     ## Open image (identifies from file content):
     img = Image.open(img_input)
@@ -46,7 +50,11 @@ def multimedia_convert_img_to_bmp(img_input, img_output=''.join(img_input.split(
 #########
 ## size = (width, height); Supported options:
 ## [(16, 16), (24, 24), (32, 32), (48, 48), (64, 64), (128, 128), (256, 256)]
-def multimedia_convert_img_to_ico(img_input, img_output=''.join(img_input.split(".")[:-1])+".ico", img_size=256):
+def multimedia_convert_img_to_ico(img_input, img_output=None, img_size=256):
+
+    ## If 'img_output' was not set by user, use the same name as 'img_input', just change extension:
+    if img_output == None:
+        img_output = ''.join(img_input.split(".")[:-1])+".ico"
 
     ## Open image (identifies from file content):
     img = Image.open(img_input)
@@ -75,7 +83,11 @@ def multimedia_convert_img_to_ico(img_input, img_output=''.join(img_input.split(
 #  JPG  #
 #########
 ## quality = 0 - 100; Not recommended to go over 95.
-def multimedia_convert_img_to_jpg(img_input, img_output=''.join(img_input.split(".")[:-1])+".jpg", img_quality=95):
+def multimedia_convert_img_to_jpg(img_input, img_output=None, img_quality=95):
+
+    ## If 'img_output' was not set by user, use the same name as 'img_input', just change extension:
+    if img_output == None:
+        img_output = ''.join(img_input.split(".")[:-1])+".jpg"
 
     ## Open image (identifies from file content):
     img = Image.open(img_input)
@@ -85,7 +97,13 @@ def multimedia_convert_img_to_jpg(img_input, img_output=''.join(img_input.split(
 
 
 
+
+
+
 ## Usage Examples:
 #multimedia_convert_img_to_bmp(HOME_DIR + "img01.jpg", HOME_DIR + "img01.bmp")
 #multimedia_convert_img_to_ico(HOME_DIR + "img01.jpg", HOME_DIR + "img01.ico", 256)
 #multimedia_convert_img_to_jpg(HOME_DIR + "img01.png", HOME_DIR + "img01.jpg", 95)
+
+
+multimedia_convert_img_to_jpg("lumine02.png", "lumine02.jpg", 95)
