2b6c370fad1a455ce38043a48cf474035782d46a
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-11-16 23:39:45 +0100

    Create getopt.c

---
+++ C/getopt.c
@@ -0,0 +1,43 @@
+#include <stdio.h>  /* printf */
+#include <getopt.h> /* getopt */
+
+/* SOURCES:
+ * https://azrael.digipen.edu/~mmead/www/Courses/CS180/getopt.html
+*/
+
+int main(int argc, char *argv[]) {
+    /* CLI parameter check */
+    int opt;
+
+    /* SYNTAX:
+     * - Disable auto error printing = put ':' before arguments.  (":abc")
+     * - By default, getopt moves all non-option arguments to end,
+     *   to prevent it use '-' at the start                       ("-:abc")
+     * - If option requires argument = put ':' after argument.    ("-:a:b:c:")
+     * - For optional option arguments = put "::" after argument. ("-:a::b::c::")
+     */
+
+    /* getopt returns -1 -> no more options -> loop terminates. */
+        while ((opt = getopt(argc, argv, "-:nr:o::")) != -1) {
+        switch (opt) {
+            case 'n':
+                printf("Option 'n' was provided.\n");
+                break;
+           case 'r':
+                printf("Option 'r' was provided with parameter '%s'.\n", optarg);
+                break;
+            /* there cannot be space between optional argument and optional option argument. ("-oOPTION") */
+            case 'o':
+                printf("Option 'o' was provided with parameter '%s'.\n", optarg ? optarg : "NULL");
+                break;
+            /* Custom Error Handling: */
+            case '?':
+                printf("ERROR! Unknown option: '%c'!\n", optopt);
+            break;
+            case ':':
+                printf("ERROR! Missing arg for '%c'!\n", optopt);
+            break;
+        }   
+    }   
+    return 0;
+}
